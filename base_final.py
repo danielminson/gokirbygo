28c23b2173c6d054077fa6761596bb7aeb46b984
Sy2k <stephaniel@al.insper.edu.br>
2019-05-10 11:37:46 -0300

    a

--- base_final.py
+++ base_final.py
@@ -1,3 +1,107 @@
+import pygame
+from pygame.locals import *
+import sys
+import math
+from os import path
+
+pygame.init()
+
+WIDTH, HEIGHT = 1440, 800
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption('Go Kirby Go')
+
+size = [1280, 720]
+WHITE = (255, 255, 255)
+black = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+img_dir = path.join(path.dirname(_file_), 'Imagens')
+
+clock = pygame.time.Clock()
+CHAO = 0
+JUMP = 1
+
+# Classe Jogador que representa a nave
+class Player(pygame.sprite.Sprite):
+    
+    # Construtor da classe.
+    def _init_(self):
+        
+        # Construtor da classe pai (Sprite).
+        pygame.sprite.Sprite._init_(self)
+        
+        # Carregando a imagem de fundo
+        player_img = pygame.image.load(path.join(img_dir, "kirby.png")).convert()
+        self.image = player_img
+        
+        # Diminuindo o tamanho da imagem.
+        self.image = pygame.transform.scale(player_img, (100, 100))
+        
+        # Deixando transparente.
+        self.image.set_colorkey(black)
+        
+        # Detalhes sobre o posicionamento.
+        self.rect = self.image.get_rect()
+        
+        # Centraliza embaixo da tela.
+        self.rect.centerx = WIDTH / 2
+        self.rect.bottom = HEIGHT -150        
+        # Velocidade do kirby
+        self.speedx = 0
+        self.speedy = 0
+        # Melhora a colisão estabelecendo um raio de um circulo
+        self.radius = 25
+        self.posX, self.posY = 50, size[1] - 180
+        self.rect.center = (self.posX, self.posY)
+        self.jumpSpeed = 10
+        self.fallSpeed = 11
+        self.slideDis = 4
+        self.jumping = False
+        self.estado = CHAO
+
+    def process_event(self, event):
+        print(event.type)
+
+        if event.type == pygame.KEYDOWN \
+            and event.key == pygame.K_SPACE \
+            and self.estado == CHAO:
+            self.speedy = -15
+            self.estado = JUMP
+
+        if self.estado == CHAO:
+            # Verifica se apertou alguma tecla.
+            if event.type == pygame.KEYDOWN:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    self.speedx = -8
+                if event.key == pygame.K_RIGHT:
+                    self.speedx = 8
+
+            # Verifica se soltou alguma tecla.
+            if event.type == pygame.KEYUP:
+                # Dependendo da tecla, altera a velocidade.
+                if event.key == pygame.K_LEFT:
+                    self.speedx = 0
+                if event.key == pygame.K_RIGHT:
+                    self.speedx = 0
+
+
+    def update(self):
+        self.rect.x += self.speedx
+        self.rect.y += self.speedy
+
+        if self.estado == JUMP:
+            self.speedy += 1
+        
+        # Mantem dentro da tela
+        if self.rect.right > WIDTH:
+            self.rect.right = WIDTH
+        if self.rect.left < 0:
+            self.rect.left = 0
+
 def redesenhafundo():
     screen.blit(fundo, (fundoX, 0)) 
     screen.blit(fundo, (fundoX2, 0))  
