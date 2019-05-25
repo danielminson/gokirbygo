12a91fb8ecf0f61115d01302996981c728fbfa52
danielminson <49560569+danielminson@users.noreply.github.com>
2019-05-25 10:33:23 -0300

    mudei o nome do arquivo e mudei o acionamento do pause

--- codigos_juntos.py
+++ codigos_juntos.py
@@ -323,8 +323,7 @@ def pause():
                 if event.key == pygame.K_p:
                     paused = True
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_r:
-                    paused = False
+                paused = False
         game_paused_img = pygame.image.load(path.join(cenarios_dir, "game_paused.png")).convert()
         game_paused_rect = game_paused_img.get_rect()
         screen.fill(BLACK)
